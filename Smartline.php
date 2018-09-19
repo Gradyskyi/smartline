e81c24594136a3cafb8d7adf22ed57593427365a
Roman <gurichna@gmail.com>
2018-09-19 11:05:13 +0300

    composer init

---
+++ Smartline.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: roman
+ * Date: 19.09.18
+ * Time: 11:03
+ */
+
+namespace gurichna\smartline;
+
+class Smartline
+{
+    public function __construct()
+    {
+        echo __CLASS__;
+    }
+}
